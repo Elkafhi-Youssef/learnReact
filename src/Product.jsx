2f38ffd54261df989535a10fd660d6897bccf25f
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-03-13 13:30:02 +0100

    create state

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,9 +4,20 @@ import React, { Component } from 'react'
 
 class Product extends Component{
 
-    state = {}
+    state = {
+        name:'youssef',
+        imgURL:"logo192.png"
+    }
     render(){
-        return(<h1>hi youssef</h1>);
+        return(
+        <div>
+            <img src={this.state.imgURL} alt="" />
+            <h1 className=''>{this.state.name}</h1>
+        </div>
+        );
     }
 }
 export default Product;
+
+// return use always ()
+// we use always className not class because class alerady reserve
